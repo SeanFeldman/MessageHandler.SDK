f9e81c54b0f5f82693d26b32720a4d7310e54de4
Yves Goeleven <yves@goeleven.com>
2014-04-06 18:35:18 +0200

    more convenience methods for container registration

--- src/MessageHandler/IContainer.cs
+++ src/MessageHandler/IContainer.cs
@@ -6,10 +6,12 @@ namespace MessageHandler
     public interface IContainer
     {
         void Register<T>();
+        void Register<T>(Func<T> factory);
         void Register<T>(Lifecycle lifecycle);
         void Register<T>(Lifecycle lifecycle, Func<T> factory );
         void Register(Type type);
         void Register(Type type, Lifecycle lifecycle);
+        void Register(Type type, Func<object> factory);
         void Register(Type type, Lifecycle lifecycle, Func<object> factory);
 
         T Resolve<T>();
