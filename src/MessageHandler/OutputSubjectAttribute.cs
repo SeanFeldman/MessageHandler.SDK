7a3865a85464a9a1b56c7e097ad9fcefb4e27d2e
Yves Goeleven <yves@goeleven.com>
2015-03-18 11:12:57 +0100

    added output subject attribute

---
+++ src/MessageHandler/OutputSubjectAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MessageHandler
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class OutputSubjectAttribute : Attribute
+    {
+        public OutputSubjectAttribute(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
