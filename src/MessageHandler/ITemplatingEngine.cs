3ce93f175737a27cddb6fd12493b9bfb961a023d
chton <bramdb@gmail.com>
2015-08-11 00:18:02 +0200

    Updated with new templating engine methods

--- src/MessageHandler/ITemplatingEngine.cs
+++ src/MessageHandler/ITemplatingEngine.cs
@@ -2,6 +2,7 @@ namespace MessageHandler
 {
     public interface ITemplatingEngine
     {
-        string Apply(string template, object message = null, object channel = null, object environment = null, object account = null);
+        string Apply(string template, object message = null, object channel = null, object environment = null, object account = null, object context = null);
+        string Apply(string template, object message, object context);
     }
 }
